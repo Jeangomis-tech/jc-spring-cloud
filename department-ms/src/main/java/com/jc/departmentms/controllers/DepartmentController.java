f83a5da4535c937287274b1c370997316a3bc497
Jean Gomis <jeangomistech2018@gmail.com>
2025-06-20 10:38:11 +0200

    Add the createDepartment method in DepartmentController

--- department-ms/src/main/java/com/jc/departmentms/controllers/DepartmentController.java
+++ department-ms/src/main/java/com/jc/departmentms/controllers/DepartmentController.java
@@ -1,4 +1,27 @@
 package com.jc.departmentms.controllers;
 
+import com.jc.departmentms.dtos.DepartmentDto;
+import com.jc.departmentms.services.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/departments")
+@AllArgsConstructor
+
 public class DepartmentController {
+    private final  DepartmentService departmentService;
+
+    @GetMapping("{code}")
+    public ResponseEntity<DepartmentDto> getDepartment(@PathVariable String code) {
+        return new ResponseEntity<DepartmentDto>(
+                departmentService.getDepartmentByCode(code), HttpStatus.OK
+        );
+    }
+    @PostMapping
+    public ResponseEntity<DepartmentDto> createDepartment(@RequestBody DepartmentDto departmentDto) {
+        return new ResponseEntity<>(departmentService.createDepartment(departmentDto), HttpStatus.CREATED);
+    }
 }
