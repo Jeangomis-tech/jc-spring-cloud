9f75dd0bb0bca0fe98f18db5c2d532516bb87fa9
Jean Gomis <jeangomistech2018@gmail.com>
2025-06-20 10:39:28 +0200

    Set the ID in DepartmentMapper

--- department-ms/src/main/java/com/jc/departmentms/mappers/DepartmentMapper.java
+++ department-ms/src/main/java/com/jc/departmentms/mappers/DepartmentMapper.java
@@ -1,4 +1,4 @@
-package com.jc.departmentms.services.mappers;
+package com.jc.departmentms.mappers;
 
 import com.jc.departmentms.dtos.DepartmentDto;
 import com.jc.departmentms.models.Department;
@@ -8,18 +8,21 @@ import org.springframework.stereotype.Component;
 public class DepartmentMapper {
 
     public  Department toEntity(DepartmentDto dto) {
-        if(dto == null) return null;
+
         Department depart = new Department();
+        if(dto.getId() != null){
+            depart.setId(dto.getId());
+        }
         depart.setCode(dto.getCode());
         depart.setDepartmentName(dto.getDepartmentName());
         return depart;
     }
 
     public  DepartmentDto toDto(Department department) {
-        if(department == null) return null;
         DepartmentDto dto = new DepartmentDto();
+        dto.setId(department.getId());
         dto.setCode(department.getCode());
-        dto.setCode(department.getDepartmentName());
+        dto.setDepartmentName(department.getDepartmentName());
         return dto;
     }
 }
